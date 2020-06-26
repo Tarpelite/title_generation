e77b1f0628b9d93ecd75103d88819819d1b4c60f
Tarpelite <tarpelite_chan@foxmail.com>
2020-06-26 14:49:17 +0800

    debug

--- src/transformers/data/datasets/language_modeling.py
+++ src/transformers/data/datasets/language_modeling.py
@@ -12,6 +12,7 @@ from ...tokenization_utils import PreTrainedTokenizer
 from ...modeling_utils import PreTrainedModel
 from ...training_args import TrainingArguments
 from .glue import glue_convert_examples_to_features
+from ..processors.utils import InputExample,InputFeatures
 import random
 import json
 import copy
@@ -151,36 +152,7 @@ class LineByLineTextDataset(Dataset):
     def __getitem__(self, i) -> torch.Tensor:
         return torch.tensor(self.examples[i], dtype=torch.long)
 
-class InputFeatures(object):
-    """
-    A single set of features of data.
 
-    Args:
-        input_ids: Indices of input sequence tokens in the vocabulary.
-        attention_mask: Mask to avoid performing attention on padding token indices.
-            Mask values selected in ``[0, 1]``:
-            Usually  ``1`` for tokens that are NOT MASKED, ``0`` for MASKED (padded) tokens.
-        token_type_ids: Segment token indices to indicate first and second portions of the inputs.
-        label: Label corresponding to the input
-    """
-
-    def __init__(self, input_ids, attention_mask=None, token_type_ids=None, label=None):
-        self.input_ids = input_ids
-        self.attention_mask = attention_mask
-        self.token_type_ids = token_type_ids
-        self.label = label
-
-    def __repr__(self):
-        return str(self.to_json_string())
-
-    def to_dict(self):
-        """Serializes this instance to a Python dictionary."""
-        output = copy.deepcopy(self.__dict__)
-        return output
-
-    def to_json_string(self):
-        """Serializes this instance to a JSON string."""
-        return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
 
 class FullyLineByLineTextDataset(Dataset):
 
@@ -218,7 +190,7 @@ class FullyLineByLineTextDataset(Dataset):
 
                 examples = []
                 for (i, line) in enumerate(lines):
-                    guid = "%s-%s" % (set_type, i)
+                    guid = "%s-%s" % ("train", i)
                     text_a = line
                     label = "0"
                     examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
