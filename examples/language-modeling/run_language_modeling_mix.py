0c6a2924f916d65fccec7925a24b82497fbac458
Tarpelite <tarpelite_chan@foxmail.com>
2020-07-29 15:14:16 +0800

    debug

--- examples/language-modeling/run_language_modeling_mix.py
+++ examples/language-modeling/run_language_modeling_mix.py
@@ -144,16 +144,7 @@ class DataTrainingArguments:
 
 def get_dataset(args: DataTrainingArguments, tokenizer: PreTrainedTokenizer,model_args:ModelArguments, evaluate=False):
     file_path = args.eval_data_file if evaluate else args.train_data_file
-    return 
-    if args.line_by_line:
-        if args.mlm_sample_times > 1:
-            return FullyLineByLineTextDataset(tokenizer=tokenizer, file_path=file_path, block_size=args.block_size, cache_dir=model_args.cache_dir)
-        else:
-            return LineByLineTextDataset(tokenizer=tokenizer, file_path=file_path, block_size=args.block_size)
-    else:
-        return TextDataset(
-            tokenizer=tokenizer, file_path=file_path, block_size=args.block_size, overwrite_cache=args.overwrite_cache
-        )
+    return MixTextDataset(tokenizer=tokenizer, file_path=file_path, block_size=args.block_size)
 
 
 def main():
