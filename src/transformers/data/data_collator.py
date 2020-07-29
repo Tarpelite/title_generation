0224bb0ed26bbc376cf3a736acfb39c7ae2b4fab
Tarpelite <tarpelite_chan@foxmail.com>
2020-07-29 20:13:46 +0800

    debug

--- src/transformers/data/data_collator.py
+++ src/transformers/data/data_collator.py
@@ -211,10 +211,7 @@ class DataCollatorForGAN(DataCollator):
 
     def collate_batch(self, examples: List[torch.Tensor]) -> Dict[str, torch.Tensor]:
         batch = self._tensorize_batch(examples)
-        if self.mlm:
-            return {"input_ids": inputs}
-        else:
-            return {"input_ids": batch}
+        return {"input_ids": batch}
 
     def _tensorize_batch(self, examples: List[torch.Tensor]) -> torch.Tensor:
         length_of_first = examples[0].size(0)
@@ -229,6 +226,40 @@ class DataCollatorForGAN(DataCollator):
                 )
             return pad_sequence(examples, batch_first=True, padding_value=self.tokenizer.pad_token_id)
 
+    def mask_tokens(self, inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+
+        if self.tokenizer.mask_token is None:
+            raise ValueError(
+                "This tokenizer does not have a mask token which is necessary for masked language modeling. Remove the --mlm flag if you want to use this tokenizer."
+            )
+
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        special_tokens_mask = [
+            self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+        ]
+        probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+        if self.tokenizer._pad_token is not None:
+            padding_mask = labels.eq(self.tokenizer.pad_token_id)
+            probability_matrix.masked_fill_(padding_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        inputs[indices_replaced] = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(len(self.tokenizer), labels.shape, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
     
     
 @dataclass
@@ -238,7 +269,9 @@ class DataCollatorForMixLM(DataCollator):
     mlm_probability: float = 0.15
 
     def collate_batch(self, examples:List) -> Dict[str, torch.Tensor]:
-        all_input_ids = torch.tensor([x[0] for x in examples], dtype=torch.long)
+        input_examples = [x[0] for x in examples]
+        all_input_ids = self._tensorize_batch(input_examples)
+        # all_input_ids = torch.tensor([x[0] for x in examples], dtype=torch.long)
 
         all_domain_labels = torch.tensor([x[1] for x in examples], dtype=torch.long)
 
